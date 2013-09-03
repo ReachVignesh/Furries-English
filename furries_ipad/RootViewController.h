c90d8dfeb82339568e29f66091cbeb9142fa66b8
sibi asokan <sibiasokan@sibis-MacBook-Pro.local>
2013-09-03 10:51:00 +0530

    Initial Commit

---
+++ furries_ipad/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  furries_ipad
+//
+//  Created by sibi asokan on 03/09/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
---
+++ furries_ipad/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  furries_ipad
+//
+//  Created by sibi asokan on 03/09/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
